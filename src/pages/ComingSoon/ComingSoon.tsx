daea37d55bc06a5154e6d900ef93c6a16a832a32
matvii419 <112835348+smartgeek419@users.noreply.github.com>
2023-05-26 03:46:51 +0300

    fixed_all_css

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 
 export default function ComingSoon() {
@@ -10,7 +11,7 @@ export default function ComingSoon() {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div className="relative flex flex-row justify-between bg-[#141a31] mt-[90px] category_bar w-full">
-        <div className="relative mx-10">
+        <div className="mx-10">
           <div className="bg-[#141a31] store flex flex-row h-[36px] items-center ">
             <p className="px-2 py-1 text-base text-white cursor-pointer hover:border-white hover:border-2" onClick={() => {
               setListShow(!listShow)
@@ -23,13 +24,18 @@ export default function ComingSoon() {
             <a href="" className="px-2 py-1 text-base text-white hover:border-white hover:border-2 list-category">Sell</a>
           </div >
           {
-            listShow && <ul className="top-[36px] left-0 absolute bg-[#141a31] w-[150px] z-[100000] flex flex-col category-box" onMouseLeave={() => { setListShow(false) }}>
+            listShow && <> <ul className="top-[0px] left-0 fixed bg-[#141a31] w-[150px] z-[100000] flex flex-col category-box h-[100vh] ">
+              <li className="flex flex-row items-center justify-center p-1 mt-2 text-base text-white background-[#989898]"><FontAwesomeIcon icon={faCircleUser} className="mr-2 text-lg" />Sign In</li>
               <li className="p-1 text-sm text-white">Today's Deals</li>
               <li className="p-1 text-sm text-white">Customer Service</li>
               <li className="p-1 text-sm text-white">Registry</li>
               <li className="p-1 text-sm text-white">Gift Card</li>
               <li className="p-1 text-sm text-white">Sell</li>
             </ul>
+              <div className="fixed top-0 left-0 bg-black opacity-70 z-[99999] w-[100vw] h-[100vh]" onClick={() => {
+                setListShow
+                  (!listShow)
+              }}></div></>
           }
         </div>
         <div className="flex items-center justify-center">
@@ -37,7 +43,7 @@ export default function ComingSoon() {
         </div>
       </div>
       <div className="flex flex-col items-center store-carousel">
-        <div>
+        <div className="relative">
           <Carousel showThumbs={false} swipeable={true}>
             <div>
               <img src="https://m.media-amazon.com/images/I/71RaBHdyGuL._SX3000_.jpg" />
@@ -58,6 +64,9 @@ export default function ComingSoon() {
               <img src="https://m.media-amazon.com/images/I/71RaBHdyGuL._SX3000_.jpg" />
             </div>
           </Carousel>
+          <div className="absolute top-0 left-0 z-20 w-full h-full gradient">
+
+          </div>
         </div>
         <div className="flex flex-wrap justify-between p-4 goods_list mt-[-320px] z-[100] item-group max-w-[1520px] m-auto mb-[320px] w-[90vw]">
           <div className="w-[23%] h-auto bg-white relative mb-4 store-item p-4 flex flex-col items-start font-medium">
